d2304c30fddad98c6e6c29d000bf0fe83710f09a
asakofujiki <a.fujiki1554@gmail.com>
2020-04-07 20:49:24 +0900

    [finish]first_app

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '63a873302f32f057e9894aebad777edaf0b699c432a273f7a60a184f2ca371279d27248ddeaa903a8aea252da23691f44b306476ebdb22ac8a49ca22f833b7da'
